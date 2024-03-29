6e9f9b534ab21d77660ca4a88f7b370ee236a07f
EunjiShin <eunji980310@gmail.com>
2024-03-30 00:26:56 +0900

    refactor: 연산식을 BiFunction을 이용해 표현하도록 수정

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -2,31 +2,23 @@ package calculator;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public enum Operator {
 
-  PLUS("+") {
-    int calculate(final int a, final int b) { return a + b; }
-  },
-  SUBTRACT("-") {
-    int calculate(final int a, final int b) { return a - b; }
-  },
-  MULTIPLY("*") {
-    int calculate(final int a, final int b) { return a * b; }
-  },
-  DIVIDE("/") {
-    int calculate(final int a, final int b) {
-      if (b == 0) throw new ArithmeticException();
-      return a / b;
-    }
-  };
-
-  abstract int calculate(final int a, final int b);
+  PLUS("+", (a, b) -> a + b),
+  SUBTRACT("-", (a, b) -> a - b),
+  MULTIPLY("*", (a, b) -> a * b),
+  DIVIDE("/", (a, b) -> {
+    if (b == 0) throw new ArithmeticException();
+    return a / b;
+  });
 
   private final String symbol;
+  private final BiFunction<Integer, Integer, Integer> formula;
 
   private static final Map<String, Operator> operatorMap =
       Collections.unmodifiableMap(
@@ -41,12 +33,17 @@ public enum Operator {
     return operator;
   }
 
-  Operator(final String symbol) {
+  Operator(final String symbol, final BiFunction<Integer, Integer, Integer> formula) {
     this.symbol = symbol;
+    this.formula = formula;
   }
 
   public String getSymbol() {
     return symbol;
+  }
+
+  public Integer calculate(final Integer a, final Integer b) {
+    return formula.apply(a, b);
   }
 
 }
